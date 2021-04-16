09b965d80f835a0243aa2ceaff294c6867573a4b
tdudkowski <tdudkowski@gmail.com>
2021-04-16 15:31:46 +0200

    update info on event

--- src/pages/z-wroclawiem-w-tle.js
+++ src/pages/z-wroclawiem-w-tle.js
@@ -5,36 +5,36 @@ import Layout from "../components/layout"
 import Banner from "../components/header-banner"
 
 const ZWroclawiemWTle = ({ data }) => {
-    // console.log(data)
-    return (
-        <Layout>
-            <Banner />
-            <section>
-                <h2>Z Wrocławiem w tle</h2>
-            </section>
-            <section className="imgandtxt">
-                <a href="http://tdudkowski.usermd.net/files/tuitam/afisz-z_wroclawiem_w_tle-marzec.jpg">
-                    <Img fluid={data.afisz_marzec.childImageSharp.fluid} className="cover" alt="afisz marzec" />
-                </a>
-                <div>
-                    <p>
-                        Wrocław jest bez wątpienia miastem literatury i ma się czym poszczycić. Nie przez przypadek został Światową Stolicą Książki UNESCO, a wiersz poety Tadeusza Różewicza stał się w 2016 roku oficjalnym hymnem i od tamtego czasu również oficjalną muzyczną wizytówką każdej kolejnej Światowej Stolicy Książki UNESCO. Liczne wydarzenia literackie skłoniły nas do przygotowania własnego projektu łączącego spotkania z autorami z tematycznymi wycieczkami i promocją niezwykłego miejsca na mapie Wrocławia – nowego Odra Centrum. Zapraszamy.
+  // console.log(data)
+  return (
+    <Layout>
+      <Banner />
+      <section>
+        <h2>Z Wrocławiem w tle</h2>
+      </section>
+      <section className="imgandtxt">
+        <a href="http://tdudkowski.usermd.net/files/tuitam/afisz-z_wroclawiem_w_tle-maj.jpg">
+          <Img fluid={data.afisz_maj.childImageSharp.fluid} className="cover" alt="afisz marzec" />
+        </a>
+        <div>
+          <p>
+            Wrocław jest bez wątpienia miastem literatury i ma się czym poszczycić. Nie przez przypadek został Światową Stolicą Książki UNESCO, a wiersz poety Tadeusza Różewicza stał się w 2016 roku oficjalnym hymnem i od tamtego czasu również oficjalną muzyczną wizytówką każdej kolejnej Światowej Stolicy Książki UNESCO. Liczne wydarzenia literackie skłoniły nas do przygotowania własnego projektu łączącego spotkania z autorami z tematycznymi wycieczkami i promocją niezwykłego miejsca na mapie Wrocławia – nowego Odra Centrum. Zapraszamy.
                 </p>
-                    <p>Cykl poprowadzi: Małgorzata Urlich-Kornacka</p>
-                    <p>25-26 marca 2021</p>
+          <p>Cykl poprowadzi: Małgorzata Urlich-Kornacka</p>
+          <p>20-21 maja 2021</p>
 
-                    <a href="https://viawroclaw.com/">viawroclaw - Małgorzata Urlich-Kornacka</a>
+          <a href="https://viawroclaw.com/">viawroclaw - Małgorzata Urlich-Kornacka</a>
 
-                </div>
-            </section>
-            <section>
+        </div>
+      </section>
+      <section>
 
-                <hr />
-                <a href="http://tdudkowski.usermd.net/files/tuitam/afisz-z_wroclawiem_w_tle.jpg">
-                    <Img fluid={data.afisz_luty.childImageSharp.fluid} className="cover" alt="afisz luty" />
-                </a>
-            </section>
-        </Layout >)
+        <hr />
+        <a href="http://tdudkowski.usermd.net/files/tuitam/afisz-z_wroclawiem_w_tle.jpg">
+          <Img fluid={data.afisz_luty.childImageSharp.fluid} className="cover" alt="afisz luty" />
+        </a>
+      </section>
+    </Layout >)
 }
 
 export const pageQuery = graphql`
@@ -47,7 +47,7 @@ export const pageQuery = graphql`
         }
       }
     }
-    afisz_marzec: file(relativePath: { eq: "afisz-z_wroclawiem_w_tle-marzec.jpg" }) {
+    afisz_maj: file(relativePath: { eq: "afisz-z_wroclawiem_w_tle-maj.jpg" }) {
         id
         childImageSharp {
           fluid(maxWidth: 640) {
